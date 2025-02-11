f8da3164d35bd30bf42c6fde9719f6dc82ce3ff2
F. Javier Zarazaga-Soria <javy@unizar.es>
2025-02-11 13:24:07 +0100

    Error introducido

--- app.test.js
+++ app.test.js
@@ -5,7 +5,7 @@ const request = supertest(app)
 describe("/ (root endpoint)", () => {
     it("debería responder un hola mundo", async () => {
         const response = await request.get("/")
-        expect(response.status).toBe(200)
+##        expect(response.status).toBe(200)
         expect(response.body.message).toBe("Hola Mundo!");        
     })
 }) 
