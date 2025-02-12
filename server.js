2785cd27b689f41549adbf2e19afb93e6c00e0cc
F. Javier Zarazaga-Soria <javy@unizar.es>
2025-02-12 11:39:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const app = require('./app')
 
 const PORT = process.env.PORT || 8080;
-server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Servidor web en el puerto ${PORT}.`);
 });
-
-server.closeAll = () => {
-  app.con.end()  // Cierra conexión de BD
-  server.close()  // Cierra servidor web
-}
-
-module.exports = server
