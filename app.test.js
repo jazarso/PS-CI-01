65d5bc133a9279e857a9bec889985dfef3b0dcd7
F. Javier Zarazaga-Soria <javy@unizar.es>
2025-02-12 11:14:45 +0100

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,11 +1,16 @@
 const app = require("./app")
+const server = require("./server")
 const supertest = require("supertest")
+
 const request = supertest(app)
 
-describe("/ (root endpoint)", () => {
-    it("debería responder un hola mundo", async () => {
-        const response = await request.get("/")
-##        expect(response.status).toBe(200)
-        expect(response.body.message).toBe("Hola Mundo!");        
-    })
-}) 
+it("Debe responder un hola mundo", async () => {
+  const response = await request.get("/")
+  expect(response.status).toBe(200)
+  expect(response.body.message).toBe("Hola Mundo!")  
+})
+
+
+afterAll(() => {        
+  server.closeAll()  
+});
